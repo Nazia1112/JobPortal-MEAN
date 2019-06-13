143a5ec90d0c074288661a45854cf22b920cf026
Nazia1112 <naziabhasin72@gmail.com>
2019-06-13 11:06:56 +0530

    Job Portal Backend

---
+++ Backend/config/db.js
@@ -0,0 +1,5 @@
+const mongoos = require('mongoose');
+
+const url='';
+const connect=mongoos.connect(url);
+exports.connect;
---
+++ Backend/routes/seekers.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../Controller/seekersControll');
+var verify = require('../routes/verifyUser'); 
+
+/* GET users listing. */
+router.post('/add',controller.addUser);
+router.post('/addfile',controller.addFile);
+router.post('/login',controller.loginUser);
+router.get('/userprofile',verify, controller.userprofile);
+router.post('/updateprofile',verify, controller.updateprofile);
+router.post('/editprofile',verify, controller.editprofile);
+router.get('/appliedcompany', verify, controller.companydata);
+module.exports = router;
